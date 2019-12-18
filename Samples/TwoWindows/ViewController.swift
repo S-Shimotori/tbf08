b11e8c8a0069b9f6d42e6e3c38f0afece8975f92
S-Shimotori <490.frostybirds@gmail.com>
2019-12-18 20:41:36 +0900

    Add a sample project

---
+++ Samples/TwoWindows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoWindows
+//
+//  Created by S-Shimotori on 2019/12/17.
+//  Copyright © 2019 S-Shimotori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
